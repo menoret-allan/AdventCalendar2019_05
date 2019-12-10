5a56298f3ebca1a11699689c13662d02cbf286d3
Allan Menoret <allan.menoret@gmail.com>
2019-12-10 12:49:34 +0100

    solved part one

--- Core/Library.fs
+++ Core/Library.fs
@@ -1,5 +1,117 @@
 ﻿namespace Core
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Computer =
+    type Mode =
+        | Position
+        | Immediate
+    
+    type Instruction =
+        | Add of (int * int * int)
+        | Mult  of (int * int * int)
+        | Input of (int * int)
+        | Output of int
+        | End
+
+    let translate (instructions: string) =
+        instructions.Split [|','|]
+        |> Seq.map(int)
+        |> Seq.toArray
+    
+    let translateBack result =
+        match result with
+        | None -> "Unknown instruction found"
+        | Some(instructions, outputs) -> "Instructions: " + (instructions |> Seq.map(string) |> String.concat ",") + "\nOutputs: " + (outputs |> Seq.map(string) |> String.concat ",")
+
+    let getValue (instructions: int []) pos =
+        instructions.[pos]
+
+    let setValue (instructions: int []) value pos =
+        instructions.[pos] <- value
+        instructions
+
+    let getModeX list place =
+        if list |> List.length < place then Position
+        else if list.[place - 1] = 0 then Position
+        else Immediate
+
+    let getMode2 getVal pos list place =
+        match getModeX list place with
+            | Position -> getVal (pos + place)
+            | Immediate -> pos + place
+
+    let findInstruction instructions pos =
+        let getVal = getValue instructions
+        let getPosition = getMode2 getVal pos
+        let intList = instructions.[pos] |> string |> Seq.rev |> Seq.map string |> Seq.map int |> Seq.toList
+        match intList with
+        | 9::9::_ -> Some(End)
+        | 1::0::rest | 1::rest -> Some(Add(getPosition rest 1 |> getVal, getPosition rest 2 |> getVal, getVal (pos + 3)))
+        | 2::0::rest | 2::rest -> Some(Mult(getPosition rest 1 |> getVal, getPosition rest 2 |> getVal, getVal (pos + 3)))
+        | 3::0::rest | 3::rest -> Some(Input(1, getPosition rest 1))
+        | 4::0::rest | 4::rest -> Some(Output(getPosition rest 1 |> getVal))
+        | _ -> None
+
+    let update instructions pos value =
+        Array.set instructions pos value
+        instructions
+
+    let interprete (instructions: int []) =
+        let rec loop (instructions: int []) (output: seq<int>) (pos: int) =
+            let insertInInstruction = setValue instructions
+            match findInstruction instructions pos with
+            | Some End -> Some (instructions, output)
+            | Some(Output(x)) -> loop instructions (Seq.append output (Seq.singleton x)) (pos+2)
+            | Some(Input(value, position)) -> loop (insertInInstruction value position) output (pos+2)
+            | Some(Add(left, right, position)) -> loop (insertInInstruction (left + right) position) output (pos+4)
+            | Some(Mult(left, right, position)) -> loop (insertInInstruction (left * right) position) output (pos+4)
+            | None -> None
+        loop instructions Seq.empty 0
+    
+    let compute (input: string) =
+        input |> translate |> interprete |> translateBack
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //let applyNewValueForInstruction instructions (noun, verb) =
+    //    let result = Array.copy instructions
+    //    Array.set result 1 noun
+    //    Array.set result 2 verb
+    //    result
+
+    //let findSpecificResult genInstruction (pair: (int * int)) =
+    //       match interprete (genInstruction pair) |> Array.item 0 with
+    //       | 19690720 -> Some pair
+    //       | _ -> None
+
+    //let findSet (input: string) =
+    //    let genInstruction = input |> translate |> applyNewValueForInstruction |> findSpecificResult
+    //    let pairs = seq {for noun in 0..100 do yield! seq {for verb in 0..100 do (noun , verb) } }
+    //    pairs |> Seq.choose(genInstruction) |> Seq.exactlyOne 
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -1,8 +1,71 @@
 module Tests
 
-open System
+open FsUnit
 open Xunit
+open Core.Computer
+
+[<Theory>]
+[<InlineData("1,0,0,0,99", "Instructions: 2,0,0,0,99")>]
+[<InlineData("2,3,0,3,99", "Instructions: 2,3,0,6,99")>]
+[<InlineData("2,4,4,5,99,0", "Instructions: 2,4,4,5,99,9801")>]
+[<InlineData("1,1,1,4,99,5,6,0,99", "Instructions: 30,1,1,4,2,5,6,0,99")>]
+[<InlineData("1002,4,3,4,33", "Instructions: 1002,4,3,4,99")>]
+[<InlineData("1101,100,-1,4,0", "Instructions: 1101,100,-1,4,99")>]
+[<InlineData("1004,2,99", "Instructions: 1004,2,99\nOutputs: 99")>]
+let ``str calculator`` (instructions:string, expectation:string) =
+    instructions |> compute |> should startWith expectation
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``read output simple instruction`` () = 
+    findInstruction [|4;4;2;3;69|] 0 |> should equal (Some (Output(69)))
+
+[<Fact>]
+let ``read output with more digit simple instruction`` () = 
+    findInstruction [|1004;4;2;3;42|] 0 |> should equal (Some (Output(42)))
+
+[<Fact>]
+let ``read output with more digit simple instruction with 1 mode`` () = 
+    findInstruction [|1104;42|] 0 |> should equal (Some (Output(42)))
+
+[<Fact>]
+let ``Check the first input instruction`` () =
+    findInstruction [|3;225|] 0 |> should equal (Some (Input(1,225)))
+
+[<Fact>]
+let ``Check the second input instruction`` () =
+    findInstruction [|3;7;1;7;6;6;1100;1|] 2 |> should equal (Some (Add(1, 1100, 6)))
+
+[<Fact>]
+let ``Check the third input instruction`` () =
+    findInstruction [|3;12;1;12;6;6;1101;1;238;12;104;0;1|] 6 |> should equal (Some (Add(1, 238, 12)))
+
+[<Fact>]
+let ``Check the fourth input instruction`` () =
+    findInstruction [|3;11;1;11;6;6;1101;1;238;225;104;0|] 10 |> should equal (Some (Output(0)))
+
+[<Theory>]
+[<InlineData("", 1)>]
+[<InlineData("", 2)>]
+[<InlineData("", 3)>]
+[<InlineData("0", 1)>]
+[<InlineData("10", 2)>]
+[<InlineData("110", 3)>]
+let ``Get mode X return Position`` (list: string) (pos: int) = 
+    let input = list |> Seq.map (string) |> Seq.map (int) |> Seq.toList 
+    let result = getModeX input pos
+    result |> should equal Position
+
+[<Theory>]
+[<InlineData("1", 1)>]
+[<InlineData("01", 2)>]
+[<InlineData("001", 3)>]
+let ``Get mode X return Immediate`` (list: string) (pos: int) = 
+    let input = list |> Seq.map (string) |> Seq.map (int) |> Seq.toList 
+    let result = getModeX input pos
+    result |> should equal Immediate
+
+
+
+
+
+
