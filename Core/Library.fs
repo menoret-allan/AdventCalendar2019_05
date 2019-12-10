cd168f29ddf4ce672e47d2c8ef0972772cfb6506
Allan Menoret <allan.menoret@gmail.com>
2019-12-10 13:50:31 +0100

    part 2 done

--- Core/Library.fs
+++ Core/Library.fs
@@ -10,6 +10,10 @@ module Computer =
         | Mult  of (int * int * int)
         | Input of (int * int)
         | Output of int
+        | Jump of int
+        | NoAction of int
+        | LessThan of (int * int)
+        | Equal of (int * int)
         | End
 
     let translate (instructions: string) =
@@ -39,7 +43,20 @@ module Computer =
             | Position -> getVal (pos + place)
             | Immediate -> pos + place
 
-    let findInstruction instructions pos =
+    let jumpIf cond value position =
+        match (value <> 0) = cond with
+        | true -> Jump(position)
+        | false -> NoAction(3)
+
+    let lessThan left right position =
+        if left < right then LessThan(1, position)
+        else LessThan(0, position)
+
+    let equalOp left right position =
+        if left = right then Equal(1, position)
+        else Equal(0, position)
+
+    let findInstruction inputValue instructions pos =
         let getVal = getValue instructions
         let getPosition = getMode2 getVal pos
         let intList = instructions.[pos] |> string |> Seq.rev |> Seq.map string |> Seq.map int |> Seq.toList
@@ -47,28 +64,36 @@ module Computer =
         | 9::9::_ -> Some(End)
         | 1::0::rest | 1::rest -> Some(Add(getPosition rest 1 |> getVal, getPosition rest 2 |> getVal, getVal (pos + 3)))
         | 2::0::rest | 2::rest -> Some(Mult(getPosition rest 1 |> getVal, getPosition rest 2 |> getVal, getVal (pos + 3)))
-        | 3::0::rest | 3::rest -> Some(Input(1, getPosition rest 1))
+        | 3::0::rest | 3::rest -> Some(Input(inputValue, getPosition rest 1))
         | 4::0::rest | 4::rest -> Some(Output(getPosition rest 1 |> getVal))
+        | 5::0::rest | 5::rest -> Some(jumpIf true (getPosition rest 1 |> getVal) (getPosition rest 2 |> getVal))
+        | 6::0::rest | 6::rest -> Some(jumpIf false (getPosition rest 1 |> getVal) (getPosition rest 2 |> getVal))
+        | 7::0::rest | 7::rest -> Some(lessThan (getPosition rest 1 |> getVal) (getPosition rest 2 |> getVal) (getVal (pos + 3)))
+        | 8::0::rest | 8::rest -> Some(equalOp (getPosition rest 1 |> getVal) (getPosition rest 2 |> getVal) (getVal (pos + 3)))
         | _ -> None
 
     let update instructions pos value =
         Array.set instructions pos value
         instructions
 
-    let interprete (instructions: int []) =
+    let interprete inputValue (instructions: int []) =
         let rec loop (instructions: int []) (output: seq<int>) (pos: int) =
             let insertInInstruction = setValue instructions
-            match findInstruction instructions pos with
+            match findInstruction inputValue instructions pos with
             | Some End -> Some (instructions, output)
             | Some(Output(x)) -> loop instructions (Seq.append output (Seq.singleton x)) (pos+2)
             | Some(Input(value, position)) -> loop (insertInInstruction value position) output (pos+2)
             | Some(Add(left, right, position)) -> loop (insertInInstruction (left + right) position) output (pos+4)
             | Some(Mult(left, right, position)) -> loop (insertInInstruction (left * right) position) output (pos+4)
+            | Some(Jump(position)) -> loop instructions output position
+            | Some(NoAction(increment)) -> loop instructions output (pos+increment)
+            | Some(LessThan(value, position)) -> loop (insertInInstruction value position) output (pos+4)
+            | Some(Equal(value, position)) -> loop (insertInInstruction value position) output (pos+4)
             | None -> None
         loop instructions Seq.empty 0
     
-    let compute (input: string) =
-        input |> translate |> interprete |> translateBack
+    let compute inputValue (instructions: string) =
+        instructions |> translate |> interprete inputValue |> translateBack
 
 
 
--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -12,36 +12,36 @@ open Core.Computer
 [<InlineData("1002,4,3,4,33", "Instructions: 1002,4,3,4,99")>]
 [<InlineData("1101,100,-1,4,0", "Instructions: 1101,100,-1,4,99")>]
 [<InlineData("1004,2,99", "Instructions: 1004,2,99\nOutputs: 99")>]
-let ``str calculator`` (instructions:string, expectation:string) =
-    instructions |> compute |> should startWith expectation
+let ``str calculator without jump/lessthan/equal instructions`` (instructions:string, expectation:string) =
+    instructions |> compute 1 |> should startWith expectation
 
 [<Fact>]
 let ``read output simple instruction`` () = 
-    findInstruction [|4;4;2;3;69|] 0 |> should equal (Some (Output(69)))
+    findInstruction 1 [|4;4;2;3;69|] 0 |> should equal (Some (Output(69)))
 
 [<Fact>]
 let ``read output with more digit simple instruction`` () = 
-    findInstruction [|1004;4;2;3;42|] 0 |> should equal (Some (Output(42)))
+    findInstruction 1 [|1004;4;2;3;42|] 0 |> should equal (Some (Output(42)))
 
 [<Fact>]
 let ``read output with more digit simple instruction with 1 mode`` () = 
-    findInstruction [|1104;42|] 0 |> should equal (Some (Output(42)))
+    findInstruction 1 [|1104;42|] 0 |> should equal (Some (Output(42)))
 
 [<Fact>]
 let ``Check the first input instruction`` () =
-    findInstruction [|3;225|] 0 |> should equal (Some (Input(1,225)))
+    findInstruction 1 [|3;225|] 0 |> should equal (Some (Input(1,225)))
 
 [<Fact>]
 let ``Check the second input instruction`` () =
-    findInstruction [|3;7;1;7;6;6;1100;1|] 2 |> should equal (Some (Add(1, 1100, 6)))
+    findInstruction 1 [|3;7;1;7;6;6;1100;1|] 2 |> should equal (Some (Add(1, 1100, 6)))
 
 [<Fact>]
 let ``Check the third input instruction`` () =
-    findInstruction [|3;12;1;12;6;6;1101;1;238;12;104;0;1|] 6 |> should equal (Some (Add(1, 238, 12)))
+    findInstruction 1 [|3;12;1;12;6;6;1101;1;238;12;104;0;1|] 6 |> should equal (Some (Add(1, 238, 12)))
 
 [<Fact>]
 let ``Check the fourth input instruction`` () =
-    findInstruction [|3;11;1;11;6;6;1101;1;238;225;104;0|] 10 |> should equal (Some (Output(0)))
+    findInstruction 1 [|3;11;1;11;6;6;1101;1;238;225;104;0|] 10 |> should equal (Some (Output(0)))
 
 [<Theory>]
 [<InlineData("", 1)>]
@@ -64,7 +64,49 @@ let ``Get mode X return Immediate`` (list: string) (pos: int) =
     let result = getModeX input pos
     result |> should equal Immediate
 
+[<Theory>]
+[<InlineData("3,9,8,9,10,9,4,9,99,-1,8", 8, "\nOutputs: 1")>] // broken
+[<InlineData("3,9,8,9,10,9,4,9,99,-1,8", 1, "\nOutputs: 0")>] // broken
+[<InlineData("3,9,8,9,10,9,4,9,99,-1,8", 42, "\nOutputs: 0")>]
+[<InlineData("3,3,1108,-1,8,3,4,3,99", 8, "\nOutputs: 1")>] // broken
+[<InlineData("3,3,1108,-1,8,3,4,3,99", 1, "\nOutputs: 0")>] // broken
+[<InlineData("3,3,1108,-1,8,3,4,3,99", 9, "\nOutputs: 0")>]
+let ``str calculator with equal instruction`` (instructions:string, input:int, expectation:string) =
+    instructions |> compute input |> should endWith expectation
 
+
+[<Theory>]
+[<InlineData("3,9,7,9,10,9,4,9,99,-1,8", 1, "\nOutputs: 1")>]
+[<InlineData("3,9,7,9,10,9,4,9,99,-1,8", -2, "\nOutputs: 1")>]
+[<InlineData("3,9,7,9,10,9,4,9,99,-1,8", 8, "\nOutputs: 0")>]
+[<InlineData("3,9,7,9,10,9,4,9,99,-1,8", 69, "\nOutputs: 0")>]
+[<InlineData("3,3,1107,-1,8,3,4,3,99", -4, "\nOutputs: 1")>]
+[<InlineData("3,3,1107,-1,8,3,4,3,99", 7, "\nOutputs: 1")>]
+[<InlineData("3,3,1107,-1,8,3,4,3,99", 8, "\nOutputs: 0")>]
+[<InlineData("3,3,1107,-1,8,3,4,3,99", 2389, "\nOutputs: 0")>]
+let ``str calculator with lessThan instructions`` (instructions:string, input:int, expectation:string) =
+    instructions |> compute input |> should endWith expectation
+
+[<Theory>]
+[<InlineData("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 0, "\nOutputs: 0")>]
+[<InlineData("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 0, "\nOutputs: 0")>]
+[<InlineData("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", 42, "\nOutputs: 1")>]
+[<InlineData("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", 69, "\nOutputs: 1")>]
+[<InlineData("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", -1, "\nOutputs: 1")>]
+[<InlineData("3,3,1105,-1,9,1101,0,0,12,4,12,99,1", -45678, "\nOutputs: 1")>]
+let ``str calculator with jump instructions`` (instructions:string, input:int, expectation:string) =
+    instructions |> compute input |> should endWith expectation
+
+
+[<Theory>]
+[<InlineData("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 7, "\nOutputs: 999")>]
+[<InlineData("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 0, "\nOutputs: 999")>]
+[<InlineData("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", -42, "\nOutputs: 999")>]
+[<InlineData("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 8, "\nOutputs: 1000")>]
+[<InlineData("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 9, "\nOutputs: 1001")>]
+[<InlineData("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", 34567, "\nOutputs: 1001")>]
+let ``ACCEPTANCE TESTS: compute with all instructions`` (instructions:string, input:int, expectation:string) =
+    instructions |> compute input |> should endWith expectation
 
 
 
